c776ec1c0b3fcb0f1c1e5bb397680e2a0570757c
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2023-06-15 09:49:30 +0200

    Initial basic React setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-function component() {
-  const element = document.createElement('div');
+import { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'Hello ' + 'webpack';
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
 
-  return element;
-}
-
-document.body.appendChild(component());
-
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(createElement('h1', {}, 'Hello from React!'));
