569782f8822d2e58b3f2ee4c58d70cbfc364b0a6
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2023-06-15 23:48:16 +0200

    Use JSX for sanity

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { createElement } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('react-app'));
 root.render(
-  createElement('h1', {}, 'Hello from React!')
+  <h1>Hello from React!</h1>
 );
